3aa646ba038bfa1e87c13a1c085a404bf6093398
unknown <nebylytsiad@gmail.com>
2022-06-18 21:04:02 +0200

    update

--- src/ApolloClient/client.js
+++ src/ApolloClient/client.js
@@ -1,11 +1,19 @@
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client";
-import { RestLink } from "apollo-link-rest";
+import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+// import { RestLink } from "apollo-link-rest";
+// import { HttpLink } from "apollo-link-http";
 
-const httplink = new HttpLink({
-    uri : "http://48p1r2roz4.sse.codesandbox.io",
-});
+// const restLink = new RestLink({
+//   endpoints: {
+//     openExchangeRate: "https://open.exchangerate-api.com/v6/",
+//   },
+// });
+
+// const httpLink = new HttpLink({
+//   uri: "https://48p1r2roz4.sse.codesandbox.io",
+// });
 
 export const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: ApolloLink.from([httplink]),
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache()
+//   link: ApolloLink.from([restLink, httpLink]),
 });
